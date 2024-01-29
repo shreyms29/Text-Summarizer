9f9b67cc39dd6e022114e8886542f1498f80be6f
ShreyasMS <shreyas.shre008@gmail.com>
2024-01-29 16:11:56 +0530

    Data validation added

--- src/textsummarizer/config/configuration.py
+++ src/textsummarizer/config/configuration.py
@@ -1,15 +1,17 @@
 from textsummarizer.constants import *
 from textsummarizer.utils.common import read_yaml, create_directories
-from textsummarizer.entity import DataIngestionConfig
+from textsummarizer.entity import (DataIngestionConfig, 
+                                   DataValidationConfig)
+
 
 
 class ConfigurationManager:
     def __init__(
         self,
-        congif_filepath = CONFIG_FILE_PATH,
+        config_filepath = CONFIG_FILE_PATH,
         params_filepath = PARAMS_FILE_PATH):
         
-        self.config = read_yaml(congif_filepath)
+        self.config = read_yaml(config_filepath)
         self.params = read_yaml(params_filepath)
         
         create_directories([self.config.artifacts_root])
@@ -28,3 +30,17 @@ class ConfigurationManager:
         )
         
         return data_ingestion_config
+    
+        
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES
+        )
+
+        return data_validation_config
